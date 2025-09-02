b31955837a2e5e271f399e38bb675dfb7666b188
Jonathas Carvalho <jc.chiavelli@unesp.br>
2025-09-02 08:16:19 -0300

    Descreva aqui o que você alterou

--- Frontend/src/environments/environment.development.ts
+++ Frontend/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api',
-  imageBaseUrl: 'http://localhost:8000',
+  apiUrl: 'http://127.0.0.1:8000/api',
+  imageBaseUrl: 'http://127.0.0.1:8000',
   //apiUrl: 'http://186.217.114.66:8000/api',
   //imageBaseUrl: 'http://186.217.114.66:8000'
   };
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  roduction: false,
-  apiUrl: 'http://localhost:8000/api',
-  imageBaseUrl: 'http://localhost:8000',
+  production: false,
+  apiUrl: 'http://127.0.0.1:8000/api',
+  imageBaseUrl: 'http://127.0.0.1:8000',
   //apiUrl: 'http://186.217.114.66:8000/api',
   //imageBaseUrl: 'http://186.217.114.66:8000',
   geminiApiKey: 'AIzaSyBu7E4T9ieYRweTHyKx-N5CEuap0hSQqgQ'
